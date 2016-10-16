b820333e4ab2463df83b71df8c079f4765e671e2
jgelinas33 <jgelinas33@comcast.net>
2016-10-16 10:32:21 -0400

    initial commit

---
+++ XMLSerialize/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace XMLSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlRootAttribute theRoot = new XmlRootAttribute();
+            theRoot.ElementName = "Stores";
+            theRoot.IsNullable = true;
+
+            // even though the StoreSurvey class is decorated with the xmlroot attribute, seems the only
+            // way this will work is if the xmlroot attribute is added directly to the serializer
+            var serializer = new XmlSerializer(typeof(List<StoreSurvey>),theRoot);
+
+            using (FileStream stream = File.OpenRead("E:\\GitProjects\\XMLSerialize\\XMLFile.txt"))
+            {
+                 List<StoreSurvey> storeSurvey = (List<StoreSurvey>)serializer.Deserialize(stream);
+            }
+            
+        }
+    }
+}
+
+// this is the xml
+//<Stores>
+//<StoreSurvey>
+//<AnnualSales>800000</AnnualSales>
+//<AnnualRevenue>80000</AnnualRevenue>
+//<BankName>United Security</BankName>
+//<BusinessType>BM</BusinessType>
+//<YearOpened>1996</YearOpened>
+//<Specialty>Mountain</Specialty>
+//<SquareFeet>21000</SquareFeet>
+//<Brands>2</Brands>
+//<Internet>ISDN</Internet>
+//<NumberEmployees>13</NumberEmployees>
+//</StoreSurvey>
+//<StoreSurvey>
+//<AnnualSales>800000</AnnualSales>
+//<AnnualRevenue>80000</AnnualRevenue>
+//<BankName>United Security</BankName>
+//<BusinessType>BM</BusinessType>
+//<YearOpened>1996</YearOpened>
+//<Specialty>Mountain</Specialty>
+//<SquareFeet>21000</SquareFeet>
+//<Brands>2</Brands>
+//<Internet>ISDN</Internet>
+//<NumberEmployees>13</NumberEmployees>
+//</StoreSurvey>
+//</Stores>
---
+++ XMLSerialize/StoreSurvey.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLSerialize
+{
+    [Serializable, XmlRoot (ElementName="Stores",IsNullable=true)]
+    public class StoreSurvey
+    {        
+        [XmlElement("AnnualSales")]        
+        public int AnnualSales { get; set; }
+
+        [XmlElement ("AnnualRevenue")]
+        public int AnnualRevenue { get; set; }
+
+        [XmlElement("BankName")]
+        public string BankName { get; set; }
+
+        [XmlElement ("BusinessType")]
+        public string BusinessType { get; set; }
+
+        [XmlElement ("YearOpened")]
+        public string YearOpened { get; set; }
+
+        [XmlElement("Specialty")]
+        public string Specialty { get; set; }
+
+        [XmlElement("SquareFeet")]
+        public string SquareFeet { get; set; }
+
+        [XmlElement("Brands")]
+        public int Brands { get; set; }
+
+        [XmlElement("Internet")]
+        public string Internet { get; set; }
+
+        [XmlElement("NumberOfEmployees")]
+        public int NumberOfEmployees { get; set; }
+    }
+}
